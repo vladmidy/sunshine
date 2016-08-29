0b64467e1253439c3ed7acb50cb9a066716ff060
vladmidy <vladimir@ciscoedge.com>
2016-08-28 22:13:02 -0400

    I have configured  the primary menu seleciton: "Settings"

--- app/src/main/java/com/ciscoedge/sunshine/app/MainActivity.java
+++ app/src/main/java/com/ciscoedge/sunshine/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.ciscoedge.sunshine.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sunshine_menu, menu);
+        return true;
+    }
+
 }
